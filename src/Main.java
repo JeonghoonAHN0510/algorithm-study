5f8c078c2e9ac82452ea9f62b7617537f1541b46
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2026-01-05 21:43:48 +0900

    2026-01-05 - 백준 9372 상근이의 여행(신장 트리 속성 이용)

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@ import java.util.*;
 
 public class Main {
 	static StringBuilder answer = new StringBuilder();
-	static List<Integer>[] graph;
-	static boolean[] visited;
-	static int result;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,42 +15,14 @@ public class Main {
 			int n = Integer.parseInt(st.nextToken());    // 국가의 수
 			int m = Integer.parseInt(st.nextToken());    // 비행기의 종류
 
-			graph = new ArrayList[n + 1];
-			for (int i = 1; i <= n; i++) {
-				graph[i] = new ArrayList<>();
-			}
-
 			for (int i = 0; i < m; i++) {
-				st = new StringTokenizer(br.readLine());
-				int u = Integer.parseInt(st.nextToken());
-				int v = Integer.parseInt(st.nextToken());
-				graph[u].add(v);
-				graph[v].add(u);
+				br.readLine();
 			}
-			visited = new boolean[n + 1];
-			result = 0;
-			bfs(1);
-			answer.append(result).append("\n");
+			answer.append(n - 1).append("\n");
 		}
 
 		bw.write(answer.toString().trim());
 		bw.flush();
 		bw.close();
-	}
-
-	public static void bfs(int start) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(start);
-		visited[start] = true;
-		while (!queue.isEmpty()) {
-			int current = queue.poll();
-			for (int next : graph[current]) {
-				if (!visited[next]) {
-					visited[next] = true;
-					queue.add(next);
-					result++;
-				}
-			}
-		}
 	}
 }
