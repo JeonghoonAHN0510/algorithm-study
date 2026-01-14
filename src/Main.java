3e76d67830ab31a9659c48dadbfb4895ac960629
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2026-01-14 20:46:04 +0900

    2026-01-14 - 백준 1149 RGB거리(복습)

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,28 @@ public class Main {
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        int[] array = new int[N];
-        int[] dp = new int[N];
+        int[][] costs = new int[N][3];
+        int[][] dp = new int[N][3];
 
-        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            array[i] = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                int cost = Integer.parseInt(st.nextToken());
+                costs[i][j] = cost;
+            }
         }
 
-        dp[0] = array[0];
-        int maxValue = array[0];
+        dp[0][0] = costs[0][0];
+        dp[0][1] = costs[0][1];
+        dp[0][2] = costs[0][2];
 
         for (int i = 1; i < N; i++) {
-            dp[i] = Math.max(dp[i - 1] + array[i], array[i]);
-            maxValue = Math.max(maxValue, dp[i]);
+            dp[i][0] = costs[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);
+            dp[i][1] = costs[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][2] = costs[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);
         }
 
-        answer.append(maxValue);
+        answer.append(Math.min(dp[N - 1][0], Math.min(dp[N - 1][1], dp[N - 1][2])));
 
         bw.write(answer.toString().trim());
         bw.flush();
