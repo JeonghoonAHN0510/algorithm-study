155e3cb1b6bc683df69a603d66c11e11514a533e
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2026-01-16 21:33:22 +0900

    2026-01-16 - 백준 2579 계단 오르기(복습)

--- src/Main.java
+++ src/Main.java
@@ -10,36 +10,28 @@ public class Main {
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        int[][] triangle = new int[N][N];
-        int[][] dp = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j <= i; j++) {
-                int cost = Integer.parseInt(st.nextToken());
-                triangle[i][j] = cost;
-            }
+        int[] stairs = new int[N + 1];
+        int[] dp = new int[N + 1];
+
+
+        for (int i = 1; i <= N; i++) {
+            int cost = Integer.parseInt(br.readLine());
+            stairs[i] = cost;
         }
 
-        dp[0][0] = triangle[0][0];
-
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (j == 0) {
-                    dp[i][j] = triangle[i][j] + dp[i - 1][j];
-                } else if (j == i) {
-                    dp[i][j] = triangle[i][j] + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = triangle[i][j] + Math.max(dp[i - 1][j], dp[i - 1][j - 1]);
-                }
-            }
+        dp[1] = stairs[1];
+        if (N >= 2) {
+            dp[2] = stairs[1] + stairs[2];
         }
 
-        int MaxValue = Integer.MIN_VALUE;
-        for (int i = 0; i < N; i++) {
-            MaxValue = Math.max(MaxValue, dp[N - 1][i]);
+        for (int i = 3; i <= N; i++) {
+            int case1 = dp[i - 2] + stairs[i];
+            int case2 = dp[i - 3] + stairs[i] + stairs[i - 1];
+
+            dp[i] = Math.max(case1, case2);
         }
 
-        answer.append(MaxValue);
+        answer.append(dp[N]);
 
         bw.write(answer.toString().trim());
         bw.flush();
