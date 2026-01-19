3f3c6fb0fa9687435a2572939a4006af55f9d92a
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2026-01-19 23:07:10 +0900

    2026-01-169 - 백준 2156 포도주 시식(복습)

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,24 @@ public class Main {
     public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        int[] stairs = new int[N + 1];
+        int[] grapes = new int[N + 1];
         int[] dp = new int[N + 1];
-
-
         for (int i = 1; i <= N; i++) {
-            int cost = Integer.parseInt(br.readLine());
-            stairs[i] = cost;
+            grapes[i] = Integer.parseInt(br.readLine());
         }
 
-        dp[1] = stairs[1];
-        if (N >= 2) {
-            dp[2] = stairs[1] + stairs[2];
+        dp[1] = grapes[1];
+        if (N > 1) {
+            dp[2] = grapes[1] + grapes[2];
         }
 
         for (int i = 3; i <= N; i++) {
-            int case1 = dp[i - 2] + stairs[i];
-            int case2 = dp[i - 3] + stairs[i] + stairs[i - 1];
+            int case1 = dp[i - 2] + grapes[i];
+            int case2 = dp[i - 3] + grapes[i] + grapes[i - 1];
 
-            dp[i] = Math.max(case1, case2);
+            dp[i] = Math.max(dp[i - 1], Math.max(case1, case2));
         }
 
         answer.append(dp[N]);
