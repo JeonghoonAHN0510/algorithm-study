deea242eefda9432bfdeedf605be33fddb0d2da1
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2026-01-20 21:54:31 +0900

    2026-01-20 - 백준 11053 가장 긴 증가하는 부분 수열(복습)

--- src/Main.java
+++ src/Main.java
@@ -7,30 +7,44 @@ public class Main {
     public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        int[] grapes = new int[N + 1];
-        int[] dp = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            grapes[i] = Integer.parseInt(br.readLine());
+        int[] array = new int[N];
+        Integer[] dp = new Integer[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int number = Integer.parseInt(st.nextToken());
+            array[i] = number;
         }
 
-        dp[1] = grapes[1];
-        if (N > 1) {
-            dp[2] = grapes[1] + grapes[2];
+        for (int i = 0; i < N; i++) {
+            dp(i, array, dp);
         }
 
-        for (int i = 3; i <= N; i++) {
-            int case1 = dp[i - 2] + grapes[i];
-            int case2 = dp[i - 3] + grapes[i] + grapes[i - 1];
-
-            dp[i] = Math.max(dp[i - 1], Math.max(case1, case2));
+        int maxValue = dp[0];
+        for (int i = 1; i < N; i++) {
+            maxValue = Math.max(maxValue, dp[i]);
         }
 
-        answer.append(dp[N]);
+        answer.append(maxValue);
 
         bw.write(answer.toString().trim());
         bw.flush();
         bw.close();
+    }
+
+    public static int dp (int N, int[] array, Integer[] dp) {
+        if (dp[N] == null) {
+            dp[N] = 1;
+
+            for (int i = N - 1; i >= 0; i--) {
+                if (array[i] < array[N]) {
+                    dp[N] = Math.max(dp[N], dp(i, array, dp) + 1);
+                }
+            }
+        }
+        return dp[N];
     }
 }
