1a5ae6042c8c333e7b7e18e95fac9d3a8c6ee0d5
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2026-01-21 21:49:39 +0900

    2026-01-21 - 백준 2559 수열(복습)

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,32 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-        int[] array = new int[N];
-        Integer[] dp = new Integer[N];
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());   // 날짜의 수
+        int K = Integer.parseInt(st.nextToken());   // 연속적인 날짜의 수
+        int[] PrefixSum = new int[N];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int number = Integer.parseInt(st.nextToken());
-            array[i] = number;
-        }
-
-        for (int i = 0; i < N; i++) {
-            dp(i, array, dp);
-        }
-
-        int maxValue = dp[0];
+        PrefixSum[0] = Integer.parseInt(st.nextToken());
         for (int i = 1; i < N; i++) {
-            maxValue = Math.max(maxValue, dp[i]);
+            int input = Integer.parseInt(st.nextToken());
+            PrefixSum[i] = input + PrefixSum[i - 1];
+        }
+
+        int[] result = new int[N - K + 1];
+        int interval = K;
+        for (int i = 0; i < N - K + 1; i++) {
+            if (i == 0) {
+                result[i] = PrefixSum[interval - 1];
+            } else {
+                result[i] = PrefixSum[interval - 1] - PrefixSum[i - 1];
+            }
+            interval++;
+        }
+
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < result.length; i++) {
+            maxValue = Math.max(maxValue, result[i]);
         }
 
         answer.append(maxValue);
@@ -33,18 +42,5 @@ public class Main {
         bw.write(answer.toString().trim());
         bw.flush();
         bw.close();
-    }
-
-    public static int dp (int N, int[] array, Integer[] dp) {
-        if (dp[N] == null) {
-            dp[N] = 1;
-
-            for (int i = N - 1; i >= 0; i--) {
-                if (array[i] < array[N]) {
-                    dp[N] = Math.max(dp[N], dp(i, array, dp) + 1);
-                }
-            }
-        }
-        return dp[N];
     }
 }
